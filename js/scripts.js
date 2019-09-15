9391c43317b54474c74f077efe7447746a05af76
*allan9595* <bohanzhang@my.unt.edu>
2019-09-15 16:54:09 -0400

    added mobile first apporach

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,44 @@ const phraseElement = $("phrase");
 let missed = 0;
 
 
+const startGame = () => {
+    if($("#phrase ul li").length !==0){
+        keyboard();
+    }
+    //start the game
+    $(".btn__reset").click(() => {
+        $("#overlay").removeClass("lose");
+        $("#overlay").removeClass("win");
+        $('#overlay').hide();
+        const phraseArray = getRandomPhraseAsArray(phrase);       
+        addPhraseToDisplay(phraseArray);
+    })
+}
+
+//reset the game 
+
+const resetGame = () => {
+    missed = 0;
+    //reset the phrase displayed
+    $("#phrase ul").remove();
+    $("#phrase").append("<ul></ul>");
+    $('#scoreboard ol li').remove();
+    $('#scoreboard ol').append(`
+        <li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>
+    `);
+    //reset the buttons keyboard
+    $("#qwerty .keyrow button").each((index, element) => {
+        $(element).removeAttr("class");
+        $(element).prop('disabled', false); //add disabled 
+    })
+}
+
 //get random phrase from the phrase array and return a phrase with splited char
 const getRandomPhraseAsArray = (arr) => {
-    //console.log(arr.length);
     //get the random number based on the array length
     let randomeNumber = Math.floor(Math.random() * Math.floor(arr.length-1));
     //console.log(randomeNumber);
@@ -57,20 +92,47 @@ const checkLetter = (btn) => {
     
 }
 
-//start the game
-$(".btn__reset").click(() => {
-    $('.start').hide();
-    const phraseArray = getRandomPhraseAsArray(phrase);
-    
-    addPhraseToDisplay(phraseArray);
-})
+//check for won 
 
+const checkWin = () => {
+    let lengthLetters = $("#phrase ul li.letter").length;
+    let lengthShows = $("#phrase ul li.letter.show").length;
+    if((lengthLetters === lengthShows) && missed <5 && (lengthLetters !== 0) && (lengthShows !==0)){
+        $("#overlay").removeClass("start");
+        $("#overlay").addClass("win");
+        $("h2.title").text("You Win!");
+        $("#overlay").show();
+        resetGame();
+    }
+
+    if((lengthLetters !== lengthShows) && missed >=5 && (lengthLetters !== 0)){
+        $("#overlay").removeClass("start");
+        $("#overlay").addClass("lose");
+        $("h2.title").text("You Lose! Good Luck Next Turn!");
+        $("#overlay").show();
+        resetGame();
+    }
+}
+
+startGame();
 //event listener on the keyboard
+
 
 $("#qwerty .keyrow button").click((e) => {
     $(e.target).addClass("chosen"); //add chosen class
-    $(e.target).attr("disabled"); //add disabled 
+    $(e.target).prop('disabled', true); //add disabled 
     
     let letterFound = checkLetter($(e.target).text()); //pass the letter to the check letter
-    console.log(letterFound);
+
+    //remove a heart if miss one 
+    if(letterFound === null){
+        $($('#scoreboard ol li')[0]).remove();
+        $('#scoreboard ol').append(`<li class="tries"><img src="images/lostHeart.png" height="35px" width="30px"></li>`);
+        missed += 1; 
+        
+    } 
+
+    checkWin();
 })
+
+
